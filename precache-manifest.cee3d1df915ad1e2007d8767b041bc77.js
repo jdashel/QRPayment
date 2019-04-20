ba4cffabdf3f8a38a4946c3cb982cc184686ee78
Jesús David Mondragón Cirio <david@adhocserv.com>
2019-04-19 21:48:50 -0500

    Inicio del proyecto.

---
+++ precache-manifest.cee3d1df915ad1e2007d8767b041bc77.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "48944fbda440677aff0b",
+    "url": "/css/app.67c28be2.css"
+  },
+  {
+    "revision": "48944fbda440677aff0b",
+    "url": "/js/app-legacy.dbb9a79b.js"
+  },
+  {
+    "revision": "ce9ffa03e069f8102d1c",
+    "url": "/js/chunk-vendors-legacy.ceaba06f.js"
+  },
+  {
+    "revision": "dd44153dce5d9beba52a414d21e2e618",
+    "url": "/index.html"
+  }
+];
---
+++ precache-manifest.e8ea139ed3cd2d1ae5dc88352f38ffe3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ec1ed9b29ea0b49a2b74",
+    "url": "/css/app.67c28be2.css"
+  },
+  {
+    "revision": "ec1ed9b29ea0b49a2b74",
+    "url": "/js/app.c115e3c3.js"
+  },
+  {
+    "revision": "c5f30aacc1b981f4650d",
+    "url": "/js/chunk-vendors.be5deb8c.js"
+  },
+  {
+    "revision": "300357131fce0056499f9a601545aaeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+];
